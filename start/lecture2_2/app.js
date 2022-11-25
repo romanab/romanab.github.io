bef71aee3c12e8086b1ba98a565025ce15444626
romanab <romanab@users.noreply.github.com>
2022-11-25 14:45:44 -0800

    Update app.js

--- start/lecture2_2/app.js
+++ start/lecture2_2/app.js
@@ -6,20 +6,20 @@ class App{
 		const container = document.createElement( 'div' );
 		document.body.appendChild( container );
     
-        this.camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.1, 100);
+        this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 100);
 		this.camera.getWorldPosition.set(0, 0, 4);
 
 		this.scene = new THREE.Scene();
 		this.scene.background = new THREE.Color( 0xaaaaaa );
-
+		
 		const ambient = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 0.3 );
 		this.scene.add( ambient );
 
 		const light = new THREE.DirectionalLight();
-		light.getWorldPosition.set( 0.2, 1, 1);
+		light.position.set( 0.2, 1, 1);
 		this.scene.add(light);
 
-		this.renderer = new THREE.WebGLRenderer({ antialias: true });
+		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 		this.renderer.setPixelRatio( window.devicePixelRatio );
 		this.renderer.setSize( window.innerWidth, window.innerHeight );
 		container.appendChild( this.renderer.domElement );
